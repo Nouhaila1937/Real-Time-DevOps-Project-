38d761bc3b495eb8c379aedab3146d2b64b65650
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-03-23 22:40:35 -0400

    routing association and getway asso

--- networking/main.tf
+++ networking/main.tf
@@ -57,8 +57,7 @@ resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
   }
 }
 
-# Public Route Table
-# il permet de bien lier le subnet public par la getway
+# Public Route Table création
 resource "aws_route_table" "dev_proj_1_public_route_table" {
   vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
   route {
@@ -70,25 +69,25 @@ resource "aws_route_table" "dev_proj_1_public_route_table" {
   }
 }
 
-# # Public Route Table and Public Subnet Association
-# resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
-#   count          = length(aws_subnet.dev_proj_1_public_subnets)
-#   subnet_id      = aws_subnet.dev_proj_1_public_subnets[count.index].id
-#   route_table_id = aws_route_table.dev_proj_1_public_route_table.id
-# }
+# Public Route Table and Public Subnet Association
+resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
+  count          = length(aws_subnet.dev_proj_1_public_subnets)
+  subnet_id      = aws_subnet.dev_proj_1_public_subnets[count.index].id
+  route_table_id = aws_route_table.dev_proj_1_public_route_table.id
+}
 
 # # Private Route Table
-# resource "aws_route_table" "dev_proj_1_private_subnets" {
-#   vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
-#   #depends_on = [aws_nat_gateway.nat_gateway]
-#   tags = {
-#     Name = "dev-proj-1-private-rt"
-#   }
-# }
+resource "aws_route_table" "dev_proj_1_private_subnets" {
+  vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  #depends_on = [aws_nat_gateway.nat_gateway]
+  tags = {
+    Name = "dev-proj-1-private-rt"
+  }
+}
 
 # # Private Route Table and private Subnet Association
-# resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
-#   count          = length(aws_subnet.dev_proj_1_private_subnets)
-#   subnet_id      = aws_subnet.dev_proj_1_private_subnets[count.index].id
-#   route_table_id = aws_route_table.dev_proj_1_private_subnets.id
-# }
+resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
+  count          = length(aws_subnet.dev_proj_1_private_subnets)
+  subnet_id      = aws_subnet.dev_proj_1_private_subnets[count.index].id
+  route_table_id = aws_route_table.dev_proj_1_private_subnets.id
+}
